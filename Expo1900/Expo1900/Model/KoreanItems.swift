6025d6e710f82190e2d411e2b6bfddb831e8a7bb
Tak <oit5538@gmail.com>
2021-04-10 18:39:09 +0900

    refactor : json 변수명 수정

--- Expo1900/Expo1900/Model/KoreanItems.swift
+++ Expo1900/Expo1900/Model/KoreanItems.swift
@@ -9,20 +9,21 @@ import Foundation
 
 struct KoreanItems: Decodable {
     enum CodingKeys: String, CodingKey {
-        case name, desc
+        case name
         case imageName = "image_name"
-        case shortDesc = "short_desc"
+        case shortDescription = "short_desc"
+        case description = "desc"
     }
     let name: String
     let imageName: String
-    let shortDesc: String
-    let desc: String
+    let shortDescription: String
+    let description: String
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         name = (try? container.decode(String.self, forKey: .name)) ?? ""
         imageName = (try? container.decode(String.self, forKey: .imageName)) ?? ""
-        shortDesc = (try? container.decode(String.self, forKey: .shortDesc)) ?? ""
-        desc = (try? container.decode(String.self, forKey: .desc)) ?? ""
+        shortDescription = (try? container.decode(String.self, forKey: .shortDescription)) ?? ""
+        description = (try? container.decode(String.self, forKey: .description)) ?? ""
     }
 }
