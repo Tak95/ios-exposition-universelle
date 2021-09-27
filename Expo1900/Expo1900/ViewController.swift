7552d4d690298562056ddd638cf7c99450ffa326
Tak <oit5538@gmail.com>
2021-09-28 00:17:37 +0900

    feat: Model과 Connect시킬 Controller 프로퍼티 생성

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -8,11 +8,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expo1900Title: UILabel!
+    @IBOutlet weak var expo1900Visitors: UILabel!
+    @IBOutlet weak var expo1900Location: UILabel!
+    @IBOutlet weak var expo1900Duration: UILabel!
+    @IBOutlet weak var expo1900Description: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
