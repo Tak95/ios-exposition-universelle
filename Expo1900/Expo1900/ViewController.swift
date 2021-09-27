096081d11a3e2d15dc4538ada01368b72a770b9e
Tak <oit5538@gmail.com>
2021-09-28 01:08:41 +0900

    feat: asset파일을 가져와 decode하는 decodeExpoData메서드 생성 / View에 나타내는 initExpoData메서드 생성

--- Expo1900/Expo1900/ViewController.swift
+++ Expo1900/Expo1900/ViewController.swift
@@ -13,10 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var expo1900Location: UILabel!
     @IBOutlet weak var expo1900Duration: UILabel!
     @IBOutlet weak var expo1900Description: UILabel!
+    var expoData: Expo1900?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        decodeExpoData()
+        initExpoData()
         // Do any additional setup after loading the view.
     }
+    
+    private func decodeExpoData() {
+        guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
+        let decoder = JSONDecoder()
+        expoData = try? decoder.decode(Expo1900.self, from: dataAsset.data)
+    }
+    
+    func initExpoData() {
+        guard let expoDataItem = expoData else { return }
+        expo1900Title.text = expoDataItem.title
+        expo1900Visitors.text = "방문객 : \(String(expoDataItem.visitors))"
+        expo1900Location.text = "개최지 : \(expoDataItem.location)"
+        expo1900Duration.text = "개최 기간 : \(expoDataItem.duration)"
+        expo1900Description.text = expoDataItem.description
+    }
+
 }
 
