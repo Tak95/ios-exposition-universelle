f38c86dfee6d93938cbfde8b549d98aa40a54698
Tak <oit5538@gmail.com>
2021-10-19 17:48:30 +0900

    feat : storyboard삭제 및 navigationController 생성, navigationBar 숨김

---
+++ Expo1900/Expo1900/Model/Expo.swift
@@ -0,0 +1,8 @@
+//
+//  Expo.swift
+//  Expo1900
+//
+//  Created by 오인탁 on 2021/10/19.
+//
+
+import Foundation
---
+++ Expo1900/Expo1900/Model/KoreanItems.swift
@@ -0,0 +1,8 @@
+//
+//  KoreanItems.swift
+//  Expo1900
+//
+//  Created by 오인탁 on 2021/10/19.
+//
+
+import Foundation
