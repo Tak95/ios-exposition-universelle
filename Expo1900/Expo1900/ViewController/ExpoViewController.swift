db67560fdb33dd3a11c6c65b03657873aa0feee2
Tak <oit5538@gmail.com>
2021-10-25 16:17:24 +0900

    feat: ExpoViewController 내부 프로퍼티 변경 및 레이아웃 메서드 구현 - 프로퍼티 클로저활용해 선언 - scroll가능한 scrollView 및 contentsView 구현 - 레이아웃 메서드 구현

--- Expo1900/Expo1900/ViewController/ExpoViewController.swift
+++ Expo1900/Expo1900/ViewController/ExpoViewController.swift
@@ -8,62 +8,146 @@ import UIKit
 
 class ExpoViewController: UIViewController {
     var expo1900: Expo?
+    let scrollView: UIScrollView = {
+        let scrollView: UIScrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        return scrollView
+    }()
     
-    private var titleLabel: UILabel!
-    private var expoImage: UIImageView!
-    private var visitorsLabel: UILabel!
-    private var locationLabel: UILabel!
-    private var durationLabel: UILabel!
-    private var descriptionLabel: UILabel!
+    let contentsView: UIView = {
+        let contentsView: UIView = UIView()
+        contentsView.translatesAutoresizingMaskIntoConstraints = false
+        return contentsView
+    }()
+    
+    private var titleLabel: UILabel = {
+        var titleLabel: UILabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title1)
+        titleLabel.numberOfLines = 2
+        return titleLabel
+    }()
+    
+    private var expoImage: UIImageView = {
+        var expoImage: UIImageView = UIImageView()
+        expoImage.translatesAutoresizingMaskIntoConstraints = false
+        expoImage.contentMode = .scaleAspectFit
+        return expoImage
+    }()
+    
+    private var visitorsLabel: UILabel = {
+        var visitorsLabel: UILabel = UILabel()
+        visitorsLabel.translatesAutoresizingMaskIntoConstraints = false
+        visitorsLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        return visitorsLabel
+    }()
+    
+    private var locationLabel: UILabel = {
+        var locationLabel: UILabel = UILabel()
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        locationLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        return locationLabel
+    }()
+    
+    private var durationLabel: UILabel = {
+        var durationLabel: UILabel = UILabel()
+        durationLabel.translatesAutoresizingMaskIntoConstraints = false
+        durationLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        return durationLabel
+    }()
+    
+    private var descriptionLabel: UILabel = {
+        var descriptionLabel: UILabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        descriptionLabel.numberOfLines = 0
+        return descriptionLabel
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+        self.view.addSubview(scrollView)
+        scrollViewConstraints()
+        contentsViewConstraints()
+        dataContraints()
         decodeData()
         initData()
-//        setupMainLayout()
-//        dataContraints()
     }
 
     func decodeData() {
         guard let dataAsset = NSDataAsset(name: "exposition_universelle_1900") else { return }
         let jsonDecoder = JSONDecoder()
-        try! jsonDecoder.decode(Expo.self, from: dataAsset.data)
+        expo1900 = try? jsonDecoder.decode(Expo.self, from: dataAsset.data)
     }
     
     func initData() {
         guard let expoData = expo1900 else { return }
         titleLabel.text = expoData.title
         expoImage.image = UIImage(named: "poster")
-        visitorsLabel.text = "\(expoData.visitors) 명"
-        locationLabel.text = expoData.location
-        durationLabel.text = expoData.duration
+        visitorsLabel.text = "방문자 : \(expoData.visitors) 명"
+        locationLabel.text = "개최지 : \(expoData.location)"
+        durationLabel.text = "개최기간 : \(expoData.duration)"
         descriptionLabel.text = expoData.description
     }
     
-    func setupMainLayout() {
-        titleLabel?.translatesAutoresizingMaskIntoConstraints = false
-        expoImage?.translatesAutoresizingMaskIntoConstraints = false
-        visitorsLabel?.translatesAutoresizingMaskIntoConstraints = false
-        locationLabel?.translatesAutoresizingMaskIntoConstraints = false
-        durationLabel?.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel?.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(titleLabel)
-        view.addSubview(expoImage)
-        view.addSubview(visitorsLabel)
-        view.addSubview(locationLabel)
-        view.addSubview(durationLabel)
-        view.addSubview(descriptionLabel)
-        
-//        titleLabel.font = .d
+    func scrollViewConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        scrollView.addSubview(contentsView)
+        contentsView.addSubview(titleLabel)
+        contentsView.addSubview(expoImage)
+        contentsView.addSubview(visitorsLabel)
+        contentsView.addSubview(locationLabel)
+        contentsView.addSubview(durationLabel)
+        contentsView.addSubview(descriptionLabel)
     }
-
+    
+    func contentsViewConstraints() {
+        NSLayoutConstraint.activate([
+            contentsView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentsView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentsView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentsView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            contentsView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor)
+        ])
+    }
+    
     func dataContraints() {
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        expoImage.centerXAnchor.constraint(equalTo: titleLabel.centerXAnchor).isActive = true
-        expoImage.centerYAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: contentsView.topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: expoImage.topAnchor, constant: -10),
+            titleLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            expoImage.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            expoImage.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            expoImage.bottomAnchor.constraint(equalTo: visitorsLabel.topAnchor, constant: -10),
+            expoImage.heightAnchor.constraint(equalToConstant: 200),
+            visitorsLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            visitorsLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            visitorsLabel.bottomAnchor.constraint(equalTo: locationLabel.topAnchor, constant: -10),
+            locationLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            locationLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            locationLabel.bottomAnchor.constraint(equalTo: durationLabel.topAnchor, constant: -10),
+            durationLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            durationLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            durationLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -10),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentsView.bottomAnchor),
+            descriptionLabel.widthAnchor.constraint(equalTo: contentsView.widthAnchor)
+        ])
+        titleLabel.textAlignment = .center
+        visitorsLabel.textAlignment = .center
+        locationLabel.textAlignment = .center
+        durationLabel.textAlignment = .center
     }
 }
-
