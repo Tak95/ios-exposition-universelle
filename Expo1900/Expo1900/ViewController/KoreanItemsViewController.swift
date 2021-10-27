628f4944461cd38cca2474aaacd91f6b2645cc91
Tak <oit5538@gmail.com>
2021-10-27 13:45:31 +0900

    feat: koreanItemsTableView선언 및 cell생성 후 타입캐스팅 / JSON 디코딩

--- Expo1900/Expo1900/ViewController/KoreanItemsViewController.swift
+++ Expo1900/Expo1900/ViewController/KoreanItemsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class KoreanItemsViewController: UIViewController {
     var koreanItemsData: [KoreanItems] = []
+    var koreanItemsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +17,24 @@ class KoreanItemsViewController: UIViewController {
         navigationController?.navigationBar.topItem?.title = "메인"
         view.backgroundColor = .white
     }
+    
+    func decodeKoreanItemsData() {
+        guard let dataAsset = NSDataAsset(name: "items") else { return }
+        let jsonDecoder = JSONDecoder()
+        koreanItemsData = try! jsonDecoder.decode([KoreanItems].self, from: dataAsset.data)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return koreanItemsData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = koreanItemsTableView.dequeueReusableCell(withIdentifier: "items", for: indexPath) as? KoreanItemsCell else { return UITableViewCell() }
+        return cell
+    }
+}
+class KoreanItemsCell: UITableViewCell {
+    var itemsTitle: UILabel!
+    var itemsImage: UIImageView!
+    var itemsShortDesc: UILabel!
 }
