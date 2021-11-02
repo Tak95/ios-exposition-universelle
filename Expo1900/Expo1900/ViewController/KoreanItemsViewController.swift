8a9d5d4e40e4904512e21dac73f01ef86615f893
Tak <oit5538@gmail.com>
2021-11-02 11:59:35 +0900

    refactor: cellStackView 삭제 및 labelStackView 내부 Content에 대한 Constraints 제거

--- Expo1900/Expo1900/ViewController/KoreanItemsViewController.swift
+++ Expo1900/Expo1900/ViewController/KoreanItemsViewController.swift
@@ -68,13 +68,12 @@ class KoreanItemsCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.contentView.addSubview(cellStackView)
-        cellStackView.addArrangedSubview(itemsImage)
-        cellStackView.addArrangedSubview(labelStackView)
+        self.addSubview(itemsImage)
+        self.addSubview(labelStackView)
         labelStackView.addArrangedSubview(itemsTitle)
         labelStackView.addArrangedSubview(itemsShortDesc)
-//        labelStackViewConstraints()
-        cellStackViewConstraints()
+        itemsImageConstraints()
+        labelStackViewConstraints()
     }
 
     required init?(coder: NSCoder) {
@@ -86,12 +85,6 @@ class KoreanItemsCell: UITableViewCell {
         labelStackView.translatesAutoresizingMaskIntoConstraints = false
         labelStackView.axis = .vertical
         return labelStackView
-    }()
-    var cellStackView: UIStackView = {
-        var cellStackView: UIStackView = UIStackView()
-        cellStackView.translatesAutoresizingMaskIntoConstraints = false
-        cellStackView.spacing = 10
-        return cellStackView
     }()
     var itemsTitle: UILabel = {
         var itemsTitle: UILabel = UILabel()
@@ -113,51 +106,21 @@ class KoreanItemsCell: UITableViewCell {
         return itemsShortDesc
     }()
     
-//    func labelStackViewConstraints() {
-//        NSLayoutConstraint.activate([
-//            labelStackView.topAnchor.constraint(equalTo: self.topAnchor),
-//            labelStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-//            labelStackView.leadingAnchor.constraint(equalTo: itemsImage.trailingAnchor),
-//            labelStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
-//        ])
-//        itemsTitleConstraints()
-//        itemsShortDescConstraints()
-//    }
-//
-//    func itemsTitleConstraints() {
-//        NSLayoutConstraint.activate([
-//            itemsTitle.topAnchor.constraint(equalTo: labelStackView.topAnchor),
-//            itemsTitle.bottomAnchor.constraint(equalTo: itemsShortDesc.topAnchor),
-//            itemsTitle.leadingAnchor.constraint(equalTo: labelStackView.leadingAnchor),
-//            itemsTitle.trailingAnchor.constraint(equalTo: labelStackView.trailingAnchor)
-//        ])
-//    }
-//
-//    func itemsShortDescConstraints() {
-//        NSLayoutConstraint.activate([
-//            itemsShortDesc.topAnchor.constraint(equalTo: itemsTitle.bottomAnchor),
-//            itemsShortDesc.bottomAnchor.constraint(equalTo: labelStackView.bottomAnchor),
-//            itemsShortDesc.leadingAnchor.constraint(equalTo: labelStackView.leadingAnchor),
-//            itemsShortDesc.trailingAnchor.constraint(equalTo: labelStackView.trailingAnchor)
-//        ])
-//    }
-    
-    func cellStackViewConstraints() {
+    func labelStackViewConstraints() {
         NSLayoutConstraint.activate([
-            cellStackView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-            cellStackView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-            cellStackView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
-            cellStackView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
+            labelStackView.topAnchor.constraint(equalTo: self.topAnchor),
+            labelStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            labelStackView.leadingAnchor.constraint(equalTo: itemsImage.trailingAnchor, constant: 15),
+            labelStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
-        itemsImageConstraints()
     }
     
     func itemsImageConstraints() {
         NSLayoutConstraint.activate([
-//            itemsImage.topAnchor.constraint(equalTo: cellStackView.topAnchor),
-//            itemsImage.bottomAnchor.constraint(equalTo: cellStackView.bottomAnchor),
-//            itemsImage.leadingAnchor.constraint(equalTo: cellStackView.leadingAnchor),
-//            itemsImage.trailingAnchor.constraint(equalTo: labelStackView.leadingAnchor),
+            itemsImage.topAnchor.constraint(equalTo: contentView.topAnchor),
+            itemsImage.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            itemsImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            itemsImage.trailingAnchor.constraint(equalTo: labelStackView.leadingAnchor,constant: -15),
             itemsImage.widthAnchor.constraint(equalToConstant: 100),
             itemsImage.heightAnchor.constraint(equalTo: itemsImage.widthAnchor)
         ])
